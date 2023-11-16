0ef5c3ae755ef8f61d2c55612f7b920403c605cc
Marwan-11 <115376022+Marwan-11@users.noreply.github.com>
2023-11-16 03:32:39 +0200

    first commit

---
+++ src/pages/HomeLayout.jsx
@@ -0,0 +1,12 @@
+import { Outlet } from 'react-router-dom';
+const HomeLayout = () => {
+  return (
+    <>
+      <nav>
+        <span className="text-4xl text-primary">Maro</span>
+      </nav>
+      <Outlet />
+    </>
+  );
+};
+export default HomeLayout;

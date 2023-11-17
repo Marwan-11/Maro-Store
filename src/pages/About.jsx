0d6a8050c282aac34c3f4d01719bfa68a9c6d6ff
Marwan-11 <115376022+Marwan-11@users.noreply.github.com>
2023-11-18 01:31:30 +0200

    hero + about + bars

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,4 +1,26 @@
 const About = () => {
-  return <div>About</div>;
+  return (
+    <>
+      <div className="flex flex-wrap gap-2 sm:gap-x-6 items-center justify-center">
+        <h1 className=" text-4xl font-bold leading-none tracking-tight sm:text-6xl">
+          We Love
+        </h1>
+        <div className="stats bg-primary shadow">
+          <div className="stat">
+            <div className="stat-title text-primary-content text-4xl font-bold tracking-widest">
+              M
+            </div>
+          </div>
+        </div>
+      </div>
+      <p className=" mt-6 leading-8 max-w-2xl mx-auto">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem tenetur
+        dicta corporis vel, officia cumque. Exercitationem labore dicta nam
+        velit ab vero aspernatur nemo et asperiores voluptate in tempore ipsum,
+        ad sed nihil aperiam soluta. Deleniti reprehenderit voluptate accusamus
+        optio dolorum molestiae ipsum blanditiis suscipit.
+      </p>
+    </>
+  );
 };
 export default About;
